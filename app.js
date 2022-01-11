fdc5ff32a40f443014bca7991a3d1287f97d6d97
AmitSheth9 <amit.sheth9@gmail.com>
2022-01-11 01:58:23 -0600

    added cors header middlew

--- app.js
+++ app.js
@@ -33,6 +33,13 @@ app.use(cors());
 app.use(bodyparser.urlencoded({extended:false}))
 app.use(bodyparser.json())
 app.use(session({ secret: 'cats', resave: false, saveUninitialized: true }))
+app.use(function (req, res, next) {
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+      next();
+    });
 
 passport.use(
     new LocalStrategy((username, password, done) => {
